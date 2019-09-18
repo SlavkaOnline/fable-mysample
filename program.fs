3e141f695d2cdc017b6df66114d5127c42d43a6d
vlopaev <vlopaev@knoema.com>
2019-09-18 15:32:44 +0500

    first

---
+++ program.fs
@@ -0,0 +1,43 @@
+﻿module App
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Browser
+open Browser.Types
+open Fable.React
+open Fable.React.Props
+
+import "*" "./styles/main.scss"
+
+type MyState = {
+    Value :int
+}
+
+let appView= 
+    FunctionComponent.Of(fun (props: {| initCount: int |}) -> 
+        let state = Hooks.useState<MyState>({Value = props.initCount})
+        
+        let up()  = 
+            state.update(fun old -> {old with Value = old.Value + 1})
+        
+        let down() = 
+            state.update(fun old -> {old with Value = old.Value - 1})
+
+        div [] [
+                button
+                    [OnClick (fun e -> up())]
+                    [str "+"]
+                div[ClassName "value"] 
+                    [str "" ; ofInt state.current.Value]
+                button
+                    [OnClick (fun e -> down())]
+                    [str "-"]
+            ]
+    )
+
+
+let render() =
+    ReactDom.render(appView({|initCount = 0|}), document.getElementById("app"))
+
+render()
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+﻿var path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./fable-mysample.fsproj",
+	output: {
+		path: path.join(__dirname, "./public"),
+		filename: "bundle.js",
+	},
+	devServer: {
+		contentBase: "./public",
+		port: 8080,
+	},
+	module:{
+		rules: [{
+			test: /\.fs(x|proj)?$/,
+			use: "fable-loader"
+		},
+		{
+			test: /\.s?[ac]ss$/,
+			use: [
+			'style-loader',
+			'css-loader',
+			'sass-loader',
+			]
+		},
+		{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		},
+		{
+			test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+			use: ["file-loader"]
+		}],
+	},
+	externals: {
+		"React": "react",
+		"ReactDOM": "react-dom",
+	}
+}
